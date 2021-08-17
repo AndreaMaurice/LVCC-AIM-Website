5cb26dec13271f5d7c7a8745bcc0d69ed4e9a82a
AndreaMaurice <andreamauricedg@gmail.com>
2021-08-17 10:27:38 +0800

    head and scripts

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// A-B-O-U-T  U-S ---------------------
 // Instructors
 var span = document.getElementsByClassName('span');
 var div = document.getElementsByClassName('div');
@@ -114,4 +115,28 @@ span1[0].onclick = ()=>{
         if (m==4) {i.style.left = "-1350px";}
         if (m<0) {m=0;}
     }
+}
+
+// G-A-L-L-E-R-Y ---------------------
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
